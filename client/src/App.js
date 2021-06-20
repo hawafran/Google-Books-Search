a4878f6be8ed8ddac6ead36400ad5c16e731b8e7
Hawa <hawa.francis@gmail.com>
2021-06-20 16:11:58 -0400

    login form

--- client/src/App.js
+++ client/src/App.js
@@ -16,21 +16,23 @@ const client = new ApolloClient({
       }
     })
   },
-  url: '/graphql'
+  uri: '/graphql'
 });
 
 function App() {
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
+    <ApolloProvider client={client}>
+        <Router>
+          <>
+            <Navbar />
+            <Switch>
+              <Route exact path='/' component={SearchBooks} />
+              <Route exact path='/saved' component={SavedBooks} />
+              <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
+            </Switch>
+          </>
+        </Router>
+    // </ApolloProvider>
   );
 }
 
