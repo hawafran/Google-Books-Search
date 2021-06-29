c2968b45348e2527a7738725cbe3ad520588bbf6
Hawa <hawa.francis@gmail.com>
2021-06-28 19:07:33 -0700

    server

--- server/server.js
+++ server/server.js
@@ -2,33 +2,31 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-const { ApolloServer } = require('apollo-server-express');
-const { typeDefs, resolvers } = require('./schemas');
-const { authMiddleware } = require('./utils/auth');
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const { authMiddleware } = require('./utils/auth')
+const { resolvers, typeDefs } = require('./schemas')
+
+const { ApolloServer } = require('apollo-server-express')
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context: authMiddleware,
-});
+  context: authMiddleware
+})
 
-server.applyMiddleware({ app });
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+server.applyMiddleware({ app })
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(` Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
