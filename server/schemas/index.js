e68dc0a61787c315bed27444c1ad1f0dd3e92808
Hawa <hawa.francis@gmail.com>
2021-06-28 19:11:25 -0700

    schemas

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,4 @@
-const resolvers = require('./resolvers.js');
-const typeDefs = require('./typeDefs.js');
+const resolvers = require('./resolvers');
+const typeDefs = require('./typeDefs');
 
 module.exports = { typeDefs, resolvers };
